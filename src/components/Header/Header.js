36d5b591b8f9426e7294d006c8346910c632a22b
hadeelsala7 <h.salah.technical@gmail.com>
2022-10-23 17:50:48 +0300

    Create Header.js

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Logo } from '../Logo/Logo';
+
+export const Header = () => {
+    return (
+        <header>
+            <Logo />
+        </header>
+    );
+};
