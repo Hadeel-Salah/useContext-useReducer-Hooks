95cc6a60c5d1ad56352857e2a048c11c48b43738
hadeelsala7 <h.salah.technical@gmail.com>
2022-10-23 17:50:54 +0300

    Create COLORS.js

---
+++ src/colors/COLORS.js
@@ -0,0 +1,21 @@
+export const LIGHT = 'Light';
+export const DARK = 'Dark';
+export const GREY = 'Grey';
+
+export const COLORS = {
+  [LIGHT]: {
+    background: '#FFFFFF',
+    accent: '#282c34',
+    logo: '#61dafb'
+  },
+  [DARK]: {
+    background: '#282c34',
+    accent: '#FFFFFF',
+    logo: '#00BCD4'
+  },
+  [GREY]: {
+    background: '#555555',
+    accent: '#fffaf0',
+    logo: '#4eebff'
+  }
+};
